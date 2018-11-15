a73768c607691d27d34d4711665866ab14216827
Stasoz <truestasoz@gmail.com>
2018-11-15 20:30:31 +0200

    Forms for add and remove elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: `./app.component.html`,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularTest';
+
+  toDoTextButton:string="Add item";
+  toDoText:string ="My to-do"; 
+  toDoTextButtonRemove="Remove all items";
+  toDoList=[];
+
+  ngOnInit(){}
+
+  AddItem()
+  {
+    this.toDoList.push(this.toDoText);
+    this.toDoText='';
+  }
+  RemoveItem()
+  {  
+    this.toDoList.length=0;   
+  }
+  RemoveByIndex(index)
+  {
+    this.toDoList.splice(this.toDoList.indexOf(index), 1);
+  }
 }
